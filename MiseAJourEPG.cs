3ab0dcdbbd5e8d0079638f7109e43463bdbe8940
Paul WOISARD <paulwoisard@gmail.com>
2017-06-17 09:15:15 +0200

    Ajout du Code

---
+++ MiseAJourEPG.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string DossierUtilisateur = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        static string DossierEPG = Path.Combine(DossierUtilisateur, "Documents", "EPG Kodi");
+        static string FichierZip = Path.Combine(DossierEPG, "complet.zip");
+        static string FichierXML = Path.Combine(DossierEPG, "complet.xml");
+        static string adresse = "http://xmltv.dtdns.net/download/complet.zip";
+        static bool TraceLigne = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lancement de la mise à jour de l'EPG");
+            if (!Directory.Exists(DossierEPG))
+            {
+                Console.WriteLine("Création du Dossier EPG dans le Dossier 'Documents'");
+                Directory.CreateDirectory(DossierEPG);
+            }
+            if (File.Exists(FichierZip))
+            {
+                Console.WriteLine("Effacement du fichier Zip précédent");
+                File.Delete(FichierZip);
+            }
+            if (File.Exists(FichierXML))
+            {
+                Console.WriteLine("Effacement du fichier XML précédent");
+                File.Delete(FichierXML);
+            }
+            Console.WriteLine("Lancement du téléchargement du nouveau fichier Zip en cours...");
+            TelechagementEnFond(adresse);
+            Thread.Sleep(60000);
+        }
+
+        public static void TelechagementEnFond(string address)
+        {
+            Uri uri = new Uri(address);
+            WebClient MonClientWeb = new WebClient();
+            // Specify that the DownloadFileCallback method gets called
+            // when the download completes.
+            MonClientWeb.DownloadFileCompleted += new AsyncCompletedEventHandler (TelechagementTermine);
+            // Specify a avancement notification handler.
+            MonClientWeb.DownloadProgressChanged += new DownloadProgressChangedEventHandler (TelechagementEnCours);
+            MonClientWeb.DownloadFileAsync(uri, FichierZip);
+        }
+
+        private static void TelechagementEnCours(object sender, DownloadProgressChangedEventArgs e)
+        {
+            try
+            {
+                if ( TraceLigne == true )
+                { 
+                    DessinerBarreDeavancementionText(e.ProgressPercentage, 100);
+                }
+            }
+            catch (Exception ex) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Echec du téléchargement : " + ex.Message); }
+        }
+        private static void DessinerBarreDeavancementionText(int avancement, int tot)
+        {
+            //draw empty avancement bar
+            TraceLigne = false;
+            Console.CursorLeft = 0;
+            Console.Write("["); //start
+            Console.CursorLeft = 32;
+            Console.Write("]"); //end
+            Console.CursorLeft = 1;
+            float RepresentationPourcentageSurTrente = 30.0f / tot;
+
+            //draw filled part
+            int position = 1;
+            for (int i = 0; i < RepresentationPourcentageSurTrente * avancement; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.CursorLeft = position++;
+                Console.Write("#");
+            }
+
+            //draw unfilled part
+            for (int i = position; i <= 31; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.CursorLeft = position++;
+                Console.Write("-");
+            }
+
+            //draw totals
+            Console.CursorLeft = 35;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(avancement.ToString() + "%" + " téléchargé(s) sur " + tot.ToString() + "%" + "                      "); //blanks at the end remove any excess
+            TraceLigne = true;
+        }
+
+        private static void TelechagementTermine(object sender, AsyncCompletedEventArgs e)
+        {
+            Thread.Sleep(500);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("");
+            Console.WriteLine("Téléchargement terminé");
+            Console.WriteLine("Extraction du fichier Zip en cours...");
+            ZipFile.ExtractToDirectory(FichierZip, DossierEPG);
+            Console.WriteLine("Finit");
+            Thread.Sleep(500);
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Mise à Jour EPG sous Windows
+
+J'utilise Kodi pour regarder la TV, avec l'Add-On "PVR Simple IPTV",
+et il y a la possiblité d'ajouter les programmes TV sous format XML.
+
+Le site internet http://xmltv.dtdns.net/ fournit un fichier XML
+compressé en Zip qui fournit l'EPG pour la France.
+
+J'ai donc écrit un morceau de code en C# qui télécharge le fichier
+et le décompresse pour qu'il soit lisible avec Kodi.
+
+Le fichier XML fonctionne pour tout logiciel utilisant l'EPG.
+
+Voici le lien de Téléchargement du binaire compilé : 
+https://drive.google.com/drive/folders/0B7PjzAwMEwKsZmVzM1I1LXZjaDA?usp=sharing
